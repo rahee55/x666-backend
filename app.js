28e4cd5573d9969b9bba4469d1d07dd514f79826
Bilal Ahmad <bilal.codemaster@gmail.com>
2026-07-12 14:40:02 +0500

    done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(sessionMiddleware);
