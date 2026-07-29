4f515b2a21bde7841ef97c57b077623a78c54e6f
Bilal Ahmad <bilal.codemaster@gmail.com>
2026-07-29 14:08:23 +0500

    done

--- games/aviator/aviator.socket.js
+++ games/aviator/aviator.socket.js
@@ -299,7 +299,6 @@ const initSocket = (server) => {
                             key: 'betPlaced',
                             betId,
                             stake,
-                            betType: data.betType || null,
                             balance: wallet.balance,
                             roundId: gameState.roundId,
                         });
@@ -333,7 +332,6 @@ const initSocket = (server) => {
                         sendWs(ws, {
                             key: 'betCancelled',
                             betId: data.betId,
-                            betType: bet.betType || data.betType || null,
                             refunded: bet.stake,
                             balance: wallet.balance,
                         });
@@ -393,7 +391,6 @@ const initSocket = (server) => {
                         sendWs(ws, {
                             key: 'cashout',
                             betId: data.betId,
-                            betType: bet.betType || data.betType || null,
                             stake: bet.stake,
                             multiplier: clientMultiplier,
                             payout,
